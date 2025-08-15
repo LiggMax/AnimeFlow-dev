52808bdfdd6efb5ddea83dc252b35edba99df2f5
LiggPlus <wenzhouli06@gmail.com>
2025-08-15 09:46:33 +0800

    初始化

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
-import 'package:AnimeFlow/router/router_config.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:media_kit/media_kit.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import './controllers/theme_controller.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // 初始化Hive
-  await Hive.initFlutter();
-  MediaKit.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -22,54 +13,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late ThemeController themeController;
-  bool _isInitialized = false;
 
   @override
   void initState() {
     super.initState();
-    themeController = ThemeController();
-    _initializeTheme();
   }
 
-  Future<void> _initializeTheme() async {
-    await themeController.initTheme();
-    if (mounted) {
-      setState(() {
-        _isInitialized = true;
-      });
-    }
-  }
-
-  // 路由
-  final _router = AppRouter().routes;
 
   @override
   Widget build(BuildContext context) {
-    if (!_isInitialized) {
-      return MaterialApp(
-        home: Scaffold(body: Center(child: CircularProgressIndicator())),
-      );
-    }
 
-    return ChangeNotifierProvider.value(
-      value: themeController,
-      child: Consumer<ThemeController>(
-        builder: (context, themeController, child) {
-          return AnimatedTheme(
-            duration: const Duration(milliseconds: 300),
-            data: themeController.currentTheme,
-            child: MaterialApp.router(
-              theme: ThemeController.lightTheme,
-              darkTheme: ThemeController.darkTheme,
-              themeMode: themeController.isDarkMode
-                  ? ThemeMode.dark
-                  : ThemeMode.light,
-              routerConfig: _router,
-            ),
-          );
-        },
-      ),
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Text('文本')
+      )
     );
   }
 }
