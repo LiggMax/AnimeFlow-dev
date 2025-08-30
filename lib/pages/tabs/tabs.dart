31c37c339b80f24cb0fda872d4eceacc91387f97
LiggPlus <wenzhouli06@gmail.com>
2025-08-30 18:22:55 +0800

    style(tabs): 优化底部导航栏选中图标样式
    - 修改首页、动态、我的页面的选中图标颜色为主题颜色

--- lib/pages/tabs/tabs.dart
+++ lib/pages/tabs/tabs.dart
@@ -138,17 +138,20 @@ class _TabsState extends State<Tabs> {
     return [
       NavigationDestination(
         icon: const Icon(Icons.home_outlined),
-        selectedIcon: const Icon(Icons.home),
+        selectedIcon: Icon(Icons.home, color: Theme.of(context).primaryColor),
         label: _pageTitles[0],
       ),
       NavigationDestination(
         icon: const Icon(Icons.timeline_outlined),
-        selectedIcon: const Icon(Icons.timeline),
+        selectedIcon: Icon(
+          Icons.timeline,
+          color: Theme.of(context).primaryColor,
+        ),
         label: _pageTitles[1],
       ),
       NavigationDestination(
         icon: const Icon(Icons.person_outline),
-        selectedIcon: const Icon(Icons.person),
+        selectedIcon: Icon(Icons.person, color: Theme.of(context).primaryColor),
         label: _pageTitles[2],
       ),
     ];
