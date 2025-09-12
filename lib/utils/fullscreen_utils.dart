1c4f2ac4aee75086a36f14fceaf614c4b177321c
LiggPlus <wenzhouli06@gmail.com>
2025-09-12 17:25:17 +0800

    feat(fullscreen_utils): 优化屏幕列数判断逻辑
    - 在判断屏幕列数的函数中增加了一个新的条件，使得在宽度小于1300 像素时显示 6 列屏幕上显示 7 列
    
    Signed-off-by: LiggPlus <wenzhouli06@gmail.com>

--- lib/utils/fullscreen_utils.dart
+++ lib/utils/fullscreen_utils.dart
@@ -7,12 +7,12 @@ class FullscreenUtils {
     if (width < 600) return 3;
     if (width < 800) return 4;
     if (width < 1000) return 5;
-    return 7; // 超大屏幕显示7列
+    if (width < 1300) return 6;
+    return 7; // 超大屏幕
   }
 
   /// 判断是否为桌面设备
   static bool isDesktop() {
     return Platform.isWindows || Platform.isMacOS || Platform.isLinux;
   }
-
 }
